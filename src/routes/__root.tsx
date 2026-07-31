bf0a9f479775a9bb97733d27b58b3fb8c0e4b4de
Yash Agrawal <agrawalyash.work@gmail.com>
2026-07-31 06:51:45 +0530

    Add Title

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -15,7 +15,7 @@ export const Route = createRootRoute({
         content: "width=device-width, initial-scale=1",
       },
       {
-        title: "TanStack Start Starter",
+        title: "Yash Agrawal: Software Engineer - Vadodara, Gujarat, India",
       },
     ],
     links: [
